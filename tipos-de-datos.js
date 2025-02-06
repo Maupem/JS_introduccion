ff9086b74e73ad46edda14fadd96bbfc97471af6
Mauricio <mau.pem99@gmail.com>
2025-02-05 22:35:38 -0600

    my first copmmit

---
+++ tipos-de-datos.js
@@ -0,0 +1,14 @@
+console.log(typeof 3.14)
+console.log(typeof 'Hola Mundo')
+console.log(typeof -666)
+console.log(typeof true)
+console.log(typeof 0)
+console.log(typeof null)
+
+var nombre = 'Mauricio'
+var edad = 25
+var gusto = 'Ver la formula 1'
+var gusto2 ='Dormir jaja'
+
+console.log(`Mi nombre es ${nombre}, y tengo ${edad} años, me gusta ${gusto} y ${gusto2}  `)
+
